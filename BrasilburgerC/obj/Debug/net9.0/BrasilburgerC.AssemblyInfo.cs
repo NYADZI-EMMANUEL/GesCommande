6155ff8788992137b17a1bc2e7d042a24e29c2ec
NYADZI-EMMANUEL <nyadziamenuveve@gmail.com>
2025-12-20 11:23:06 +0000

    Fix: correction .csproj dans Dockerfile

--- BrasilburgerC/obj/Debug/net9.0/BrasilburgerC.AssemblyInfo.cs
+++ BrasilburgerC/obj/Debug/net9.0/BrasilburgerC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrasilburgerC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a40717080b32265c38658ca8aaf4ae14e552029")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+553c327d372914dee8603d4afdddbabcc45280d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrasilburgerC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrasilburgerC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
