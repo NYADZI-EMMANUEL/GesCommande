17e504ee873cbcda61989c3b6658532b74a31f79
NYADZI-EMMANUEL <nyadziamenuveve@gmail.com>
2026-02-28 12:24:52 +0000

    création du data et connection à la base de donnée

--- BrasilburgerC/obj/Debug/net9.0/BrasilburgerC.AssemblyInfo.cs
+++ BrasilburgerC/obj/Debug/net9.0/BrasilburgerC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrasilburgerC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c29f5523f82765f9d293a1387375f975cce59822")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16346ee8a497e26146fc2dccadfc3d3c2fd60645")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrasilburgerC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrasilburgerC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
